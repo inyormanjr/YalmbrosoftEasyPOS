d0920ef7a620daf8939c18a7d27f07ab2757b379
inyormanjr <ronilojunioryap@gmail.com>
2022-10-19 14:59:12 +1100

    include inventory trans to item creation, update

--- EPOSManagementWebAPI/controllers/itemController.js
+++ EPOSManagementWebAPI/controllers/itemController.js
@@ -44,6 +44,19 @@ exports.create = asyncHandler(async (req, res, next) => {
     delete variant._id;
   });
   const item = await Item.create(req.body);
+  for (const variant of item.variants) {
+    if(variant.quantity && variant.quantity > 0)
+     await createInventoryTrans(
+       'StockIn',
+       item.name,
+       variant,
+       0,
+       variant.quantity,
+       variant.quantity,
+       req,
+       'Initial'
+     );
+  }
   res.status(201).json({
     success: true,
     data: item,
@@ -53,10 +66,24 @@ exports.create = asyncHandler(async (req, res, next) => {
 exports.update = asyncHandler(async (req, res, next) => {
 
   req.body.variants.forEach((variant) => {
-    console.log(variant);
     if(variant._id == null)
         delete variant._id;
   });
+
+  for (const variant of req.body.variants) {
+      console.log(variant._id);
+    if (variant._id === undefined && (variant.quantity && variant.quantity > 0))
+      await createInventoryTrans(
+        'StockIn',
+        req.body.name,
+        variant,
+        0,
+        variant.quantity,
+        variant.quantity,
+        req,
+        'Initial'
+      );
+  }
   const item = await Item.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
     runValidators: true,
@@ -91,22 +118,36 @@ exports.updateSingleQuantity = asyncHandler(async (req, res, next) => {
            },
          }
       );
-  const newInventoryTrans = new InventoryTransaction({
-    type: inventoryTransactionType,
-    itemName: inventory.item.name,
-    variant: inventory.variant,
+  await createInventoryTrans(
+    inventoryTransactionType,
+    inventory.item.name,
+    inventory.variant,
     previousQuantity,
+    inventory.variant.quantity,
     quantity,
-    newQuantity: inventory.variant.quantity,
-    company: req.user.companyId,
-    creator: req.user.username,
-    remarks: remarks ?? 'n/a',
-  });
-   await InventoryTransaction.create(newInventoryTrans);
+    req,
+    remarks
+  );
 
    res.status(200).json({ success: true, data: {} });
 
 });
+
+createInventoryTrans = async function (inventoryTransactionType, itemName, variant, previousQuantity,quantity,newQuantity,  req, remarks) {
+  const newInventoryTrans = new InventoryTransaction({
+    type: inventoryTransactionType,
+    itemName,
+    variant,
+    previousQuantity,
+    quantity,
+    newQuantity,
+    company: req.user.companyId,
+    creator: req.user.username,
+    remarks: remarks ?? 'n/a',
+  });
+
+  await InventoryTransaction.create(newInventoryTrans);
+};
 
 exports.getStockMovement = asyncHandler(async (req, res, next) => {
   res.status(200).json(res.advancedResults);
